fd04c723a53a8bb34078d0b7a9ffdcda378d21ec
JKamsker <11245306+JKamsker@users.noreply.github.com>
2024-05-13 23:11:17 +0200

    Add Migrations

---
+++ Infrastructure/Data/Migrations/20240513201912_mig_mandatory_type_default_none.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tributech.SensorManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_mandatory_type_default_none : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "MandatoryMetadataItems",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "none",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "MandatoryMetadataItems",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldDefaultValue: "none");
+        }
+    }
+}
